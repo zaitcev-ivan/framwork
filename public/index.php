6b747598391dbe224ed23b29c36f0b7b232366ab
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-01-20 12:51:37 +0400

    Переход на объекты Request и Response от Zend Diactoros, удаление собственных классов и тестов

--- public/index.php
+++ public/index.php
@@ -1,25 +1,25 @@
 <?php
 
-use Framework\Http\RequestFactory;
-use Framework\Http\Response;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\ServerRequestFactory;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 # Initialization
 
-$request = RequestFactory::fromGlobals();
+$request = ServerRequestFactory::fromGlobals();
 
 # Action
 $name = $request->getQueryParams()['name'] ?? 'Guest';
 
-$response = (new Response('Hello, ' . $name . '!'))
+$response = (new HtmlResponse('Hello, ' . $name . '!'))
     ->withHeader('X-Developer', 'Zaicev');
 
 # Sending
 header('HTTP/1.0' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
-foreach ($response->getHeaders() as $name => $value) {
-    header($name . ':' . $value);
+foreach ($response->getHeaders() as $name => $values) {
+    header($name . ':' . implode(', ', $values));
 }
 echo $response->getBody();
 
