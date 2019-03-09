a095f80fd9cc03425ffe7b5c6c92567fb5751cd6
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-03-09 21:11:29 +0400

    Вынесение отдельных экшенов в анонимные функции

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
@@ -16,27 +17,45 @@ $request = ServerRequestFactory::fromGlobals();
 # Action
 
 $path = $request->getUri()->getPath();
+$action = null;
 
 if ($path === '/') {
-    $name = $request->getQueryParams()['name'] ?? 'Guest';
-    $response = new HtmlResponse('Hello, ' . $name . '!');
+    $action = function (ServerRequestInterface $request) {
+        $name = $request->getQueryParams()['name'] ?? 'Guest';
+        return new HtmlResponse('Hello, ' . $name . '!');
+    };
 } elseif ($path === '/about') {
-    $response = new HtmlResponse('I am a simple page');
+    $action = function () {
+        return new HtmlResponse('I am a simple page');
+    };
 } elseif ($path === '/blog') {
-    $response = new JsonResponse([
-        ['id' => 2, 'title' => 'The Second Post'],
-        ['id' => 1, 'title' => 'The First Post'],
-    ]);
+    $action = function () {
+        return new JsonResponse([
+            ['id' => 2, 'title' => 'The Second Post'],
+            ['id' => 1, 'title' => 'The First Post'],
+        ]);
+    };
 } elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
-    $id = $matches['id'];
-    if ($id > 2) {
-        $response = new JsonResponse(['error' => 'Undefined page'], 404);
-    } else {
-        $response = new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
-    }
+
+    $request = $request->withAttribute('id', $matches['id']);
+
+    $action = function (ServerRequestInterface $request) {
+        $id = $request->getAttribute('id');
+        if ($id > 2) {
+            return new JsonResponse(['error' => 'Undefined page'], 404);
+        }
+        return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    };
 } else {
     $response = new JsonResponse(['error' => 'Undefined page'], 404);
 }
+
+if ($action) {
+    $response = $action($request);
+} else {
+    $response = new JsonResponse(['error' => 'Undefined page'], 404);
+}
+
 
 
 # Postprocessing
