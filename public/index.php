e8664cc670dc457380de72ec749e3bac6ccd0eb3
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-03-09 18:34:31 +0400

    Примитивная реализация маршрутизации

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
@@ -11,14 +12,20 @@ require 'vendor/autoload.php';
 
 $request = ServerRequestFactory::fromGlobals();
 
-# Preprocessing
-if (preg_match('#json#i', $request->getHeader('Content-Type'))) {
-    $request = $request->withParsedBody(json_decode($request->getBody()->getContents()));
-}
 
 # Action
-$name = $request->getQueryParams()['name'] ?? 'Guest';
-$response = new HtmlResponse('Hello, ' . $name . '!');
+
+$path = $request->getUri()->getPath();
+
+if ($path === '/') {
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
+    $response = new HtmlResponse('Hello, ' . $name . '!');
+} elseif ($path === '/about') {
+    $response = new HtmlResponse('I am a simple page');
+} else {
+    $response = new JsonResponse(['error' => 'Undefined page'], 404);
+}
+
 
 # Postprocessing
 $response = $response->withHeader('X-Developer', 'Zaicev');
