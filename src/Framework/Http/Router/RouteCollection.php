f736fa3db5b6a301d0510b97e956276c339f656f
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-03-10 20:53:12 +0400

    Рефакторинг роутера

--- src/Framework/Http/Router/RouteCollection.php
+++ src/Framework/Http/Router/RouteCollection.php
@@ -9,19 +9,24 @@ class RouteCollection
      */
     private $routes = [];
 
+    public function addRoute(Route $route): void
+    {
+        $this->routes[] = $route;
+    }
+
     public function any($name, $pattern, $handler, array $tokens = []): void
     {
-        $this->routes[] = new Route($name, $pattern, $handler, [], $tokens);
+        $this->addRoute(new Route($name, $pattern, $handler, [], $tokens));
     }
 
     public function get($name, $pattern, $handler, array $tokens = []): void
     {
-        $this->routes[] = new Route($name, $pattern, $handler, ['GET'], $tokens);
+        $this->addRoute(new Route($name, $pattern, $handler, ['GET'], $tokens));
     }
 
     public function post($name, $pattern, $handler, array $tokens = []): void
     {
-        $this->routes[] = new Route($name, $pattern, $handler, ['POST'], $tokens);
+        $this->addRoute(new Route($name, $pattern, $handler, ['POST'], $tokens));
     }
 
     /**
