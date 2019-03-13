f2e3d211f157a738cb3fcccb871c17428d6d69b7
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-03-13 20:12:43 +0400

    Добавление middleware по расчету времени выполнения кода ProfilerMiddleware

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use App\Http\Action\Blog\IndexAction;
 use App\Http\Action\Blog\ShowAction;
 use App\Http\Action\CabinetAction;
 use App\Http\Action\HelloAction;
+use App\Http\Middleware\ProfilerMiddleware;
 use Aura\Router\RouterContainer;
 use Framework\Http\ActionResolver;
 use Framework\Http\Router\AuraRouterAdapter;
@@ -29,10 +30,13 @@ $routes = $aura->getMap();
 $routes->get('home', '/', HelloAction::class);
 $routes->get('about', '/about', AboutAction::class);
 $routes->get('cabinet', '/cabinet', function (ServerRequestInterface $request) use ($params) {
+    $profiler = new ProfilerMiddleware();
     $auth = new BasicAuthMiddleware($params['users']);
     $cabinet = new CabinetAction();
-    return $auth($request, function (ServerRequestInterface $request) use ($cabinet) {
-        return $cabinet($request);
+    return $profiler($request, function (ServerRequestInterface $request) use ($auth, $cabinet) {
+        return $auth($request, function (ServerRequestInterface $request) use ($cabinet) {
+            return $cabinet($request);
+        });
     });
 });
 $routes->get('blog', '/blog', IndexAction::class);
