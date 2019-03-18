4ebcc0bdca50706236be02af1b369c57f334f458
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-03-18 20:20:15 +0400

    Вынесение обработчика BasicAuthMiddleware в отдельную настройку для закрытия авторизацией любых путей в /cabinet

--- public/index.php
+++ public/index.php
@@ -34,10 +34,7 @@ $routes = $aura->getMap();
 
 $routes->get('home', '/', HelloAction::class);
 $routes->get('about', '/about', AboutAction::class);
-$routes->get('cabinet', '/cabinet', [
-    new BasicAuthMiddleware($params['users']),
-    CabinetAction::class,
-]);
+$routes->get('cabinet', '/cabinet', CabinetAction::class);
 $routes->get('blog', '/blog', IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', ShowAction::class)->tokens(['id' => '\d+']);
 
@@ -49,6 +46,7 @@ $app->pipe(new ErrorHandlerMiddleware($params['debug']));
 $app->pipe(CredentialsMiddleware::class);
 $app->pipe(ProfilerMiddleware::class);
 $app->pipe(new RouteMiddleware($router));
+$app->pipe('cabinet', new BasicAuthMiddleware($params['users']));
 $app->pipe(new DispatchMiddleware($resolver));
 
 ### Running
