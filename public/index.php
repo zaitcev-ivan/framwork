8082e0afa108df6924d3188953f0406f1fe68d99
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-03-19 22:45:57 +0400

    Помещение всех остальных сервисов в контейнер

--- public/index.php
+++ public/index.php
@@ -13,10 +13,10 @@ use App\Http\Middleware\ProfilerMiddleware;
 use Framework\Container\Container;
 use Framework\Http\Middleware\DispatchMiddleware;
 use Framework\Http\Middleware\RouteMiddleware;
-use Aura\Router\RouterContainer;
 use Framework\Http\Application;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
+use Framework\Http\Router\Router;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
@@ -32,6 +32,14 @@ $container->set('config', [
     'users' => ['admin' => 'password'],
 ]);
 
+$container->set(Application::class, function (Container $container) {
+    return new Application(
+        $container->get(MiddlewareResolver::class),
+        new NotFoundHandler(),
+        new Response()
+    );
+});
+
 $container->set(BasicAuthMiddleware::class, function (Container $container) {
     return new BasicAuthMiddleware($container->get('config')['users']);
 });
@@ -39,30 +47,44 @@ $container->set(ErrorHandlerMiddleware::class, function (Container $container) {
     return new ErrorHandlerMiddleware($container->get('config')['debug']);
 });
 
+$container->set(DispatchMiddleware::class, function (Container $container) {
+    return new DispatchMiddleware($container->get(MiddlewareResolver::class));
+});
+
+$container->set(MiddlewareResolver::class, function () {
+    return new MiddlewareResolver();
+});
+
+$container->set(RouteMiddleware::class, function (Container $container) {
+    return new RouteMiddleware($container->get(Router::class));
+});
+
+$container->set(Router::class, function () {
+    $aura = new Aura\Router\RouterContainer();
+    $routes = $aura->getMap();
+    $routes->get('home', '/', HelloAction::class);
+    $routes->get('about', '/about', AboutAction::class);
+    $routes->get('cabinet', '/cabinet', CabinetAction::class);
+    $routes->get('blog', '/blog', IndexAction::class);
+    $routes->get('blog_show', '/blog/{id}', ShowAction::class)->tokens(['id' => '\d+']);
+    return new AuraRouterAdapter($aura);
+});
+
 ### Initialization
-$aura = new RouterContainer();
-$routes = $aura->getMap();
 
-$routes->get('home', '/', HelloAction::class);
-$routes->get('about', '/about', AboutAction::class);
-$routes->get('cabinet', '/cabinet', CabinetAction::class);
-$routes->get('blog', '/blog', IndexAction::class);
-$routes->get('blog_show', '/blog/{id}', ShowAction::class)->tokens(['id' => '\d+']);
-
-$router = new AuraRouterAdapter($aura);
-$resolver = new MiddlewareResolver();
-$app = new Application($resolver, new NotFoundHandler(), new Response());
+/** @var Application $app */
+$app = $container->get(Application::class);
 
 $app->pipe($container->get(ErrorHandlerMiddleware::class));
 $app->pipe(CredentialsMiddleware::class);
 $app->pipe(ProfilerMiddleware::class);
-$app->pipe(new RouteMiddleware($router));
+$app->pipe($container->get(Framework\Http\Middleware\RouteMiddleware::class));
 $app->pipe('cabinet', $container->get(BasicAuthMiddleware::class));
-$app->pipe(new DispatchMiddleware($resolver));
+$app->pipe($container->get(Framework\Http\Middleware\DispatchMiddleware::class));
 
 ### Running
 $request = ServerRequestFactory::fromGlobals();
-$response = $app->run($request, new Response(), new Response());
+$response = $app->run($request, new Response());
 
 # Sending
 $emitter = new SapiEmitter();
