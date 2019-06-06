14e4894f7487d9594538498a792ff27152601d5d
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-06-06 19:45:08 +0400

    Реализация автоматического создания объектов с помощью фабрики ReflectionBasedAbstractFactory компонента Zend Service Manager

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Http\Action\AboutAction;
-use App\Http\Action\Blog\IndexAction;
-use App\Http\Action\Blog\ShowAction;
-use App\Http\Action\CabinetAction;
-use App\Http\Action\HelloAction;
 use App\Http\Middleware;
 use Framework\Http\Application;
-use Framework\Http\Middleware\DispatchMiddleware;
-use Framework\Http\Middleware\RouteMiddleware;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
@@ -16,14 +9,8 @@ use Psr\Container\ContainerInterface;
 
 return [
     'dependencies' => [
-        'invokables' => [
-            Middleware\CredentialsMiddleware::class,
-            Middleware\ProfilerMiddleware::class,
-            HelloAction::class,
-            AboutAction::class,
-            CabinetAction::class,
-            IndexAction::class,
-            ShowAction::class,
+        'abstract_factories' => [
+            Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
         ],
         'factories' => [
             Application::class => function (ContainerInterface $container) {
@@ -45,12 +32,6 @@ return [
             },
             Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
                 return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
-            },
-            DispatchMiddleware::class => function (ContainerInterface $container) {
-                return new DispatchMiddleware($container->get(MiddlewareResolver::class));
-            },
-            RouteMiddleware::class => function (ContainerInterface $container) {
-                return new RouteMiddleware($container->get(Router::class));
             },
         ],
     ],
