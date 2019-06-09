2146ee9a8127951910caf5cb957fbd57a3ffdfeb
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-06-09 20:22:19 +0400

    Добавление bootstrap-шаблонов для страниц

--- tests/App/Http/Action/HelloActionTest.php
+++ tests/App/Http/Action/HelloActionTest.php
@@ -5,7 +5,6 @@ namespace Tests\App\Http\Action;
 use App\Http\Action\HelloAction;
 use Framework\Template\TemplateRenderer;
 use PHPUnit\Framework\TestCase;
-use Zend\Diactoros\ServerRequest;
 
 class HelloActionTest extends TestCase
 {
@@ -17,19 +16,11 @@ class HelloActionTest extends TestCase
         $this->renderer = new TemplateRenderer('C:/projects/framework/templates');
     }
 
-    public function testGuest(): void
+    public function test(): void
     {
         $action = new HelloAction($this->renderer);
-        $request = new ServerRequest();
-        $response = $action($request);
+        $response = $action();
         self::assertEquals(200, $response->getStatusCode());
-        self::assertContains('Hello, Guest!', $response->getBody()->getContents());
-    }
-    public function testJohn(): void
-    {
-        $action = new HelloAction($this->renderer);
-        $request = (new ServerRequest())->withQueryParams(['name' => 'John']);
-        $response = $action($request);
-        self::assertContains('Hello, John!', $response->getBody()->getContents());
+        self::assertContains('Hello!', $response->getBody()->getContents());
     }
 }
