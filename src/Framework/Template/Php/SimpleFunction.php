2226618779139dbc4f87c9a7c5daadf1bbf3c544
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-06-10 22:43:35 +0400

    Передача конфигурации расширения шаблонизатора с использованием объектов

---
+++ src/Framework/Template/Php/SimpleFunction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Framework\Template\Php;
+
+class SimpleFunction
+{
+    public $name;
+    public $callback;
+    public $needRenderer;
+
+    /**
+     * SimpleFunction constructor.
+     * @param string $name
+     * @param callable $callback
+     * @param bool $needRenderer
+     */
+    public function __construct(string $name, callable $callback, bool $needRenderer = false)
+    {
+        $this->name = $name;
+        $this->callback = $callback;
+        $this->needRenderer = $needRenderer;
+    }
+}
