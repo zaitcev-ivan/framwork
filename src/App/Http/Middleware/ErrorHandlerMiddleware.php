4278752cc49b22c4e9a93fc0a7f67e1e91eeee45
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-06-16 22:31:37 +0400

    Переход на PSR middleware во всех классах приложения

--- src/App/Http/Middleware/ErrorHandlerMiddleware.php
+++ src/App/Http/Middleware/ErrorHandlerMiddleware.php
@@ -3,10 +3,13 @@
 namespace App\Http\Middleware;
 
 use Framework\Template\TemplateRenderer;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 
-class ErrorHandlerMiddleware
+class ErrorHandlerMiddleware implements MiddlewareInterface
 {
     private $debug;
     private $template;
@@ -16,10 +19,10 @@ class ErrorHandlerMiddleware
         $this->template = $template;
         $this->debug = $debug;
     }
-    public function __invoke(ServerRequestInterface $request, callable $next)
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         try {
-            return $next($request);
+            return $handler->handle($request);
         } catch (\Throwable $e) {
             $view = $this->debug ? 'error/error-debug' : 'error/error';
             return new HtmlResponse($this->template->render($view, [
