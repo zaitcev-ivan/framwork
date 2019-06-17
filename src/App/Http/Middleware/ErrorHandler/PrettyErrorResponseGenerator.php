035a6fad3f2526e5ed325d3af4ed75f950917f69
Ivan Zaytcev <ii.zaytcev@gmail.com>
2019-06-17 21:52:39 +0400

    Удаление зависимостей генераторов шаблонов ошибок от реализации ZendDiactors

--- src/App/Http/Middleware/ErrorHandler/PrettyErrorResponseGenerator.php
+++ src/App/Http/Middleware/ErrorHandler/PrettyErrorResponseGenerator.php
@@ -5,8 +5,6 @@ namespace App\Http\Middleware\ErrorHandler;
 use Framework\Template\TemplateRenderer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\Response;
-use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Stratigility\Utils;
 
 /**
@@ -16,11 +14,13 @@ use Zend\Stratigility\Utils;
 class PrettyErrorResponseGenerator implements ErrorResponseGenerator
 {
     private $template;
+    private $response;
     private $views;
 
-    public function __construct(TemplateRenderer $template, array $views)
+    public function __construct(TemplateRenderer $template, ResponseInterface $response, array $views)
     {
         $this->template = $template;
+        $this->response = $response;
         $this->views = $views;
     }
 
@@ -31,11 +31,15 @@ class PrettyErrorResponseGenerator implements ErrorResponseGenerator
      */
     public function generate(\Throwable $e, ServerRequestInterface $request): ResponseInterface
     {
-        $code = Utils::getStatusCode($e, new Response());
-        return new HtmlResponse($this->template->render($this->getView($code), [
-            'request' => $request,
-            'exception' => $e,
-        ]), $code);
+        $code = Utils::getStatusCode($e, $this->response);
+        $response = $this->response->withStatus($code);
+        $response
+            ->getBody()
+            ->write($this->template->render($this->getView($code), [
+                'request' => $request,
+                'exception' => $e,
+            ]));
+        return $response;
     }
 
     private function getView($code): string
